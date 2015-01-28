9d8dc7c11608409b911d97ce6d88817206521b3a
Florian <Florian@10.11.8.112>
2015-01-28 19:30:00 +0100

    servlet home

--- eTrade-Web/src/main/java/fr/dauphine/etrade/servlet/Home.java
+++ eTrade-Web/src/main/java/fr/dauphine/etrade/servlet/Home.java
@@ -10,6 +10,9 @@ import javax.servlet.http.HttpServletResponse;
 
 public class Home extends HttpServlet {
 
+	private static final long serialVersionUID = 6575782921956558199L;
+
+
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
