8e05ac8df4ba93fc6886ee54637b322d929d5c19
lupflamind <eugen.tiganu@gmail.com>
2015-03-13 14:18:15 +0100

    Commit

--- eTrade_Web/src/main/java/fr/dauphine/etrade/managedbean/OrdreManagedBean.java
+++ eTrade_Web/src/main/java/fr/dauphine/etrade/managedbean/OrdreManagedBean.java
@@ -11,14 +11,22 @@ import javax.faces.context.FacesContext;
 
 import fr.dauphine.etrade.api.ServicesOrdre;
 import fr.dauphine.etrade.api.ServicesPortefeuille;
+import fr.dauphine.etrade.model.DirectionOrdre;
 import fr.dauphine.etrade.model.Ordre;
 import fr.dauphine.etrade.model.Portefeuille;
+import fr.dauphine.etrade.model.Produit;
+import fr.dauphine.etrade.model.Role;
+import fr.dauphine.etrade.model.Societe;
+import fr.dauphine.etrade.model.StatusOrdre;
+import fr.dauphine.etrade.model.TypeOrdre;
+import fr.dauphine.etrade.model.Utilisateur;
 
 @ManagedBean
 @RequestScoped
 public class OrdreManagedBean implements Serializable {
 
 	private static final long serialVersionUID = 1L;
+	private Ordre ordre;
 
 	@EJB
 	private ServicesOrdre so;
@@ -27,7 +35,7 @@ public class OrdreManagedBean implements Serializable {
 	private ServicesPortefeuille sp;
 	
 	private static Logger LOG = Logger.getLogger(OrdreManagedBean.class.getName());
-	private Portefeuille p = sp.getPortefeuilleByUserEmail(FacesContext.getCurrentInstance().getExternalContext().getUserPrincipal().getName());
+	private Portefeuille p; //= sp.getPortefeuilleByUserEmail(FacesContext.getCurrentInstance().getExternalContext().getUserPrincipal().getName());
 	/**
 	 * This method deletes an order from the database.
 	 * It is called when an user wants to delete it.
@@ -57,4 +65,16 @@ public class OrdreManagedBean implements Serializable {
 		return so.allPendingOrdres(p.getIdPortefeuille());
 	}
 	
+	public Ordre getOrdre() {
+		if (ordre==null){
+			ordre = new Ordre();
+			ordre.setProduit(new Produit());
+			ordre.getProduit().setSociete(new Societe());
+			ordre.setPortefeuille(new Portefeuille());
+			ordre.setTypeOrdre(new TypeOrdre());
+			ordre.setDirectionOrdre(new DirectionOrdre());
+			ordre.setStatusOrdre(new StatusOrdre());
+		}
+        return ordre;
+    }
 }
---
+++ eTrade_ejb/src/main/java/fr/dauphine/etrade/api/ServicesProduit.java
@@ -0,0 +1,8 @@
+package fr.dauphine.etrade.api;
+
+import javax.ejb.Remote;
+
+@Remote
+public interface ServicesProduit {
+
+}
