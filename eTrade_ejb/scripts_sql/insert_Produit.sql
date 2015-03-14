02f19821633b372d23c3cf97d744b9cd25a6a18c
utilisateur <utilisateur@10.11.5.247>
2015-03-14 19:16:39 +0100

    fichier sql

---
+++ eTrade_ejb/scripts_sql/insert_Produit.sql
@@ -0,0 +1,3 @@
+INSERT INTO `Produit` (`IdProduit`, `IdTypeProduit`, `IdSociete`) VALUES
+(1, 1, 2),
+(2, 2, 2);
---
+++ eTrade_ejb/scripts_sql/insert_Societe.sql
@@ -0,0 +1,7 @@
+INSERT INTO `Societe` (`IdSociete`, `Name`) VALUES
+(2, 'Atos'),
+(NULL, 'Axa'),
+(NULL, 'BNP Paribas'),
+(NULL, 'CACIB'),
+(NULL, 'Société Générale'),
+(NULL, 'Test');
