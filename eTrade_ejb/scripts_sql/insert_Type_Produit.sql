6c823f7878abf8027a34bab700d14dae2a4a3245
lupflamind <eugen.tiganu@gmail.com>
2015-03-15 16:44:25 +0100

    commit

--- eTrade_ejb/scripts_sql/insert_Type_Produit.sql
+++ eTrade_ejb/scripts_sql/insert_Type_Produit.sql
@@ -1,3 +1,4 @@
 INSERT INTO `Type_Produit` (`IdTypeProduit`, `Libelle`) VALUES
 (1, 'Action'),
-(2, 'OptionA100K50T2m');
+(2, 'Option'),
+(3, 'Obligation'),
--- eTrade_ejb/src/main/java/fr/dauphine/etrade/model/Ordre.java
+++ eTrade_ejb/src/main/java/fr/dauphine/etrade/model/Ordre.java
@@ -29,7 +29,7 @@ import javax.persistence.TemporalType;
  */
 @Entity
 @Table(name = "Ordre")
-@NamedQuery(query="SELECT o FROM Ordre o LEFT JOIN FETCH o.directionOrdre LEFT JOIN FETCH o.statusOrdre LEFT JOIN FETCH o.typeOrdre LEFT JOIN FETCH o.portefeuille LEFT JOIN FETCH o.produit LEFT JOIN FETCH o.produit.societe LEFT JOIN FETCH o.produit.typeProduit WHERE o.statusOrdre.idStatusOrder = ?1 AND o.portefeuille.idPortefeuille=?2", name = "QUERY_ORDRE_STATUS")
+@NamedQuery(query="SELECT o FROM Ordre o JOIN FETCH o.directionOrdre JOIN FETCH o.statusOrdre JOIN FETCH o.typeOrdre JOIN FETCH o.portefeuille JOIN FETCH o.produit JOIN FETCH o.produit.societe JOIN FETCH o.produit.typeProduit WHERE o.statusOrdre.idStatusOrder = ?1 AND o.portefeuille.idPortefeuille=?2", name = "QUERY_ORDRE_STATUS")
 public class Ordre implements java.io.Serializable {
 
 	public static final String QUERY_ORDRE_STATUS = "QUERY_ORDRE_STATUS";
